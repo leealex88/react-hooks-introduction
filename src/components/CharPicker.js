8bd1a79294d8c6204ff9cced6eaada0b0b8b57b6
Ola Skoczylas <42297184+leealex88@users.noreply.github.com>
2020-03-16 14:27:02 +0000

    push
    push

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './CharPicker.css';
 
-class CharPicker extends Component {
-  state = { characters: [], isLoading: false };
+const CharPicker = props => {
+  const [loadedChars, setLoadedChars] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
 
-  componentDidMount() {
-    this.setState({ isLoading: true });
+  //   state = { characters: [], isLoading: false };
+  useEffect(() => {
+    console.log('useEffect runs');
+    setIsLoading(true);
     fetch('https://swapi.co/api/people')
       .then(response => {
         if (!response.ok) {
@@ -16,34 +19,38 @@ class CharPicker extends Component {
       })
       .then(charData => {
         const selectedCharacters = charData.results.slice(0, 5);
-        this.setState({
-          characters: selectedCharacters.map((char, index) => ({
+
+        setIsLoading(false);
+        setLoadedChars(
+          selectedCharacters.map((char, index) => ({
             name: char.name,
             id: index + 1
-          })),
-          isLoading: false
-        });
+          }))
+        );
       })
       .catch(err => {
         console.log(err);
+        setIsLoading(false);
       });
-  }
+  }, []);
 
-  render() {
+ 
+
+
     let content = <p>Loading characters...</p>;
-
+ 
     if (
-      !this.state.isLoading &&
-      this.state.characters &&
-      this.state.characters.length > 0
+      !isLoading &&
+      loadedChars &&
+      loadedChars.length > 0
     ) {
       content = (
         <select
-          onChange={this.props.onCharSelect}
-          value={this.props.selectedChar}
-          className={this.props.side}
+          onChange={props.onCharSelect}
+          value={props.selectedChar}
+          className={props.side}
         >
-          {this.state.characters.map(char => (
+          { loadedChars.map(char => (
             <option key={char.id} value={char.id}>
               {char.name}
             </option>
@@ -51,13 +58,77 @@ class CharPicker extends Component {
         </select>
       );
     } else if (
-      !this.state.isLoading &&
-      (!this.state.characters || this.state.characters.length === 0)
+      !isLoading &&
+      (! loadedChars || loadedChars.length === 0)
     ) {
       content = <p>Could not fetch any data.</p>;
     }
     return content;
-  }
 }
 
 export default CharPicker;
+
+
+// import React, { Component } from 'react';
+
+// import './CharPicker.css';
+
+// class CharPicker extends Component {
+//   state = { characters: [], isLoading: false };
+
+//   componentDidMount() {
+//     this.setState({ isLoading: true });
+//     fetch('https://swapi.co/api/people')
+//       .then(response => {
+//         if (!response.ok) {
+//           throw new Error('Failed to fetch.');
+//         }
+//         return response.json();
+//       })
+//       .then(charData => {
+//         const selectedCharacters = charData.results.slice(0, 5);
+//         this.setState({
+//           characters: selectedCharacters.map((char, index) => ({
+//             name: char.name,
+//             id: index + 1
+//           })),
+//           isLoading: false
+//         });
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
+
+//   render() {
+//     let content = <p>Loading characters...</p>;
+
+//     if (
+//       !this.state.isLoading &&
+//       this.state.characters &&
+//       this.state.characters.length > 0
+//     ) {
+//       content = (
+//         <select
+//           onChange={this.props.onCharSelect}
+//           value={this.props.selectedChar}
+//           className={this.props.side}
+//         >
+//           {this.state.characters.map(char => (
+//             <option key={char.id} value={char.id}>
+//               {char.name}
+//             </option>
+//           ))}
+//         </select>
+//       );
+//     } else if (
+//       !this.state.isLoading &&
+//       (!this.state.characters || this.state.characters.length === 0)
+//     ) {
+//       content = <p>Could not fetch any data.</p>;
+//     }
+//     return content;
+//   }
+// }
+
+// export default CharPicker;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,37 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Summary from './Summary';
 
-class Character extends Component {
-  state = { loadedCharacter: {}, isLoading: false };
+const Character = props => {
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate');
-    return (
-      nextProps.selectedChar !== this.props.selectedChar ||
-      nextState.loadedCharacter.id !== this.state.loadedCharacter.id ||
-      nextState.isLoading !== this.state.isLoading
-    );
-  }
+ const [loadedCharacter, setLoadedCharacter] = useState({})
+ const [isLoading, setIsLoading] = useState(false)
 
-  componentDidUpdate(prevProps) {
-    console.log('Component did update');
-    if (prevProps.selectedChar !== this.props.selectedChar) {
-      this.fetchData();
-    }
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('shouldComponentUpdate');
+  //   return (
+  //     nextProps.selectedChar !== this.props.selectedChar ||
+  //     nextState.loadedCharacter.id !== this.state.loadedCharacter.id ||
+  //     nextState.isLoading !== this.state.isLoading
+  //   );
+  // }
 
-  componentDidMount() {
-    this.fetchData();
-  }
 
-  fetchData = () => {
+  const fetchData = () => {
     console.log(
       'Sending Http request for new character with id ' +
-        this.props.selectedChar
+        props.selectedChar
     );
-    this.setState({ isLoading: true });
-    fetch('https://swapi.co/api/people/' + this.props.selectedChar)
+    setIsLoading(true)
+    fetch('https://swapi.co/api/people/' + props.selectedChar)
       .then(response => {
         if (!response.ok) {
           throw new Error('Could not fetch person!');
@@ -40,7 +32,7 @@ class Character extends Component {
       })
       .then(charData => {
         const loadedCharacter = {
-          id: this.props.selectedChar,
+          id: props.selectedChar,
           name: charData.name,
           height: charData.height,
           colors: {
@@ -50,36 +42,52 @@ class Character extends Component {
           gender: charData.gender,
           movieCount: charData.films.length
         };
-        this.setState({ loadedCharacter: loadedCharacter, isLoading: false });
+        setIsLoading(false)
+        setLoadedCharacter(loadedCharacter)
       })
       .catch(err => {
         console.log(err);
+        setIsLoading(false)
       });
   };
 
-  componentWillUnmount() {
-    console.log('Too soon...');
-  }
+   // componentDidUpdate(prevProps) {
+  //   console.log('Component did update');
+  //   if (prevProps.selectedChar !== this.props.selectedChar) {
+  //     this.fetchData();
+  //   }
+  // }
+  
+  useEffect(() => {
+    fetchData()
+  }, [])
 
-  render() {
+  
+ 
+
+
+  // componentWillUnmount() {
+  //   console.log('Too soon...');
+  // }
+
+
     let content = <p>Loading Character...</p>;
 
-    if (!this.state.isLoading && this.state.loadedCharacter.id) {
+    if (!isLoading && loadedCharacter.id) {
       content = (
         <Summary
-          name={this.state.loadedCharacter.name}
-          gender={this.state.loadedCharacter.gender}
-          height={this.state.loadedCharacter.height}
-          hairColor={this.state.loadedCharacter.colors.hair}
-          skinColor={this.state.loadedCharacter.colors.skin}
-          movieCount={this.state.loadedCharacter.movieCount}
+          name={loadedCharacter.name}
+          gender={loadedCharacter.gender}
+          height={loadedCharacter.height}
+          hairColor={loadedCharacter.colors.hair}
+          skinColor={loadedCharacter.colors.skin}
+          movieCount={loadedCharacter.movieCount}
         />
       );
-    } else if (!this.state.isLoading && !this.state.loadedCharacter.id) {
+    } else if (!isLoading && !loadedCharacter.id) {
       content = <p>Failed to fetch character.</p>;
     }
     return content;
-  }
 }
 
 export default Character;
